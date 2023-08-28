a90f69a54e4383268840ee54eb5e9669f2e926e9
nrm33n <84393679+nrm33n@users.noreply.github.com>
2023-08-28 15:17:29 +0400

    committing public copy files

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Optional
+
+class contactForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    subject = StringField("Subject", validators=[DataRequired()])
+    message = StringField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send")
+
+
+class filterForm(FlaskForm):
+    search = StringField('', validators = [Optional()])
+    partofspeech = SelectField('Part of Speech', choices=[('all', 'All'), ('pronoun', 'Pronouns'), ('noun', 'Nouns'), ('verb', 'Verbs'), ('adjective', 'Adjectives'), ('preposition'), ('Prepositions'), ('adverb'), ('Adverbs'), ('culture'), ('Misc.')], validators = [Optional()])
+    submit = SubmitField("Go")
